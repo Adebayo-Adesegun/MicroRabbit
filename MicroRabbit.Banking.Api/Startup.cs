ecc7bb8e7a16c746a6f8c7b541e06f592691f787
Adesegun Adebayo <shegibambo@gmail.com>
2020-01-28 08:20:53 +0100

    DataContext and Some Migration Issues

--- MicroRabbit.Banking.Api/Startup.cs
+++ MicroRabbit.Banking.Api/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +28,17 @@ namespace MicroRabbit.Banking.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ResgisterServices(services);
+            services.AddDbContext<BankingDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("BankingDbConnection"));
+            });
             services.AddControllers();
+        }
+
+        private void ResgisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private BankingDbContext _context;
+        public AccountRepository(BankingDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Account> GetllAccounts()
+        {
+            return _context.Accounts;
+        }
+    }
+}
